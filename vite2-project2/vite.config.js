e4916bd7ef678230ac54874c4141b656ac1f9b16
krishop90 <krishmungalpara007@gmail.com>
2025-03-06 18:11:03 +0530

    done

--- vite2-project2/vite.config.js
+++ vite2-project2/vite.config.js
@@ -1,11 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://classcloud.onrender.com', // Backend URL
+        target: 'http://localhost:5001', 
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // Remove the `/api` prefix from the path
       },
     },
   },
